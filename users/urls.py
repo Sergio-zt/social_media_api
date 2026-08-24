2b079a860d079c636946e5f51831fd1f9041651c
Sergio-zt <fop.gso@gmail.com>
2026-08-24 18:15:17 -0400

    create app users adding models, serializaers and views

---
+++ users/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+
+from users.views import UserRegistrationView, UserLogoutView, UserViewSet
+
+# Router for ViewSets
+router = DefaultRouter()
+router.register(r'profiles', UserViewSet, basename='profile')
+
+urlpatterns = [
+    # Auth endpoints
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', obtain_auth_token, name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+    
+    # Profile endpoints (list, retrieve, update)
+    path('', include(router.urls)),
+]
