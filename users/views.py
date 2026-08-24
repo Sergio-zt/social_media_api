382384b9f3906b23855e0b3f844f7abe96b1d800
Sergio-zt <fop.gso@gmail.com>
2026-08-24 18:40:42 -0400

    Create posts app implement models, viesws, serializers and permissions

--- users/views.py
+++ users/views.py
@@ -9,6 +9,9 @@ from django.contrib.auth import get_user_model
 from users.serializers import UserRegistrationSerializer, UserSerializer
 from users.permissions import IsOwnerOrReadOnly
 
+from rest_framework.decorators import action
+from .models import Follow
+
 User = get_user_model()
 
 
@@ -58,12 +61,96 @@ class UserViewSet(mixins.RetrieveModelMixin,
                   viewsets.GenericViewSet):
     """
     ViewSet for viewing, searching, and updating user profiles.
-    - We use mixins to prevent creation via this endpoint (registration handles that).
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
-    
-    # Adding search capabilities
     filter_backends = [filters.SearchFilter]
-    search_fields = ['email', 'bio']  # Allows searching by email or bio
+    search_fields = ['email', 'bio']
+
+    # --- НОВЫЙ КОД НИЖЕ ---
+
+    @action(detail=True, methods=['post'])
+    def follow(self, request, pk=None):
+        """Endpoint to follow a user."""
+        target_user = self.get_object() # Gets the user we want to follow
+
+        if target_user == request.user:
+            return Response(
+                {"detail": "You cannot follow yourself."}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        # get_or_create prevents errors if the user clicks "follow" twice
+        follow_obj, created = Follow.objects.get_or_create(
+            follower=request.user, 
+            following=target_user
+        )
+        
+        if not created:
+            return Response(
+                {"detail": "You are already following this user."}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+            
+        return Response(
+            {"detail": "Successfully followed."}, 
+            status=status.HTTP_201_CREATED
+        )
+
+    @action(detail=True, methods=['post'])
+    def unfollow(self, request, pk=None):
+        """Endpoint to unfollow a user."""
+        target_user = self.get_object()
+        
+        # Try to find the follow relationship and delete it
+        deleted_count, _ = Follow.objects.filter(
+            follower=request.user, 
+            following=target_user
+        ).delete()
+        
+        if deleted_count:
+            return Response(
+                {"detail": "Successfully unfollowed."}, 
+                status=status.HTTP_200_OK
+            )
+            
+        return Response(
+            {"detail": "You are not following this user."}, 
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    @action(detail=True, methods=['get'])
+    def followers(self, request, pk=None):
+        """Endpoint to get a list of users following the specified user."""
+        user = self.get_object()
+        
+        # Get IDs of all followers
+        follower_ids = Follow.objects.filter(following=user).values_list('follower_id', flat=True)
+        # Fetch the User objects
+        followers_qs = User.objects.filter(id__in=follower_ids)
+        
+        # Paginate and serialize using the existing UserSerializer
+        page = self.paginate_queryset(followers_qs)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(followers_qs, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['get'])
+    def following(self, request, pk=None):
+        """Endpoint to get a list of users the specified user is following."""
+        user = self.get_object()
+        
+        following_ids = Follow.objects.filter(follower=user).values_list('following_id', flat=True)
+        following_qs = User.objects.filter(id__in=following_ids)
+        
+        page = self.paginate_queryset(following_qs)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(following_qs, many=True)
+        return Response(serializer.data)
