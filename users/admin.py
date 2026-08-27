a1dc74f306272acd57288542db659161a2ffc88b
Sergio-zt <fop.gso@gmail.com>
2026-08-27 11:58:34 -0400

    Dockerize with celeru scheduler

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "is_staff", "is_superuser")
+    search_fields = ("email",)
+    list_filter = ("is_staff", "is_superuser", "is_active")
